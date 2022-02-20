8a027dac99fd456a374ff27e03967f5b59ddf799
DheVam <vanshidheeraj07@gmail.com>
2022-02-20 12:59:01 +0530

    prompt added

--- Password-Generator/index.js
+++ Password-Generator/index.js
@@ -3,6 +3,7 @@ var password = "";
 function generatePassword() {
     console.log(Math.random().toFixed(2)*100);
     password = Math.random().toString(36).slice(2) + Math.random().toString(36).slice(2);
+    prompt("Copy to clipboard: Ctrl+C, Enter", password);
     document.getElementById("password").innerHTML = password;
 }
 
